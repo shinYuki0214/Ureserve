703a7de4a4a978e650c604d4f226e2d6f0508512
岩田真次郎 <iwata@gurukun-web.com>
2023-01-11 08:43:40 +0900

    first

---
+++ resources/views/livewire-test/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    @livewireStyles
+</head>
+<body>
+    livewireテスト
+    <div>
+        @if(session()->has('message'))
+            <div class="alert alert-success">
+                {{session('message')}}
+            </div>
+        @endif
+    </div>
+    @livewire('counter')
+    @livewireScripts
+</body>
+</html>
---
+++ resources/views/livewire-test/register.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+     <!-- Scripts -->
+     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @livewireStyles
+</head>
+<body>
+    livewireテスト
+    <livewire:register />
+    @livewireScripts
+</body>
+</html>
